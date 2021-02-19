463539e4c3adf108de03610edad82542fe950380
SantonioTheFirst <santonio123@mail.ru>
2021-02-19 23:22:15 +0200

    different groups added to json

--- Bot.py
+++ Bot.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[1]:
 
 
 import json, requests
@@ -13,9 +13,9 @@ class Bot:
         assert len(filename) > 0, 'Пустое имя файла!'
         self.filename : str = filename
         self.base_url = 'https://api.telegram.org/bot1617354859:AAF_oiTlv9VYrjAbep1RJpXHkap0MPKfsBs/'
-        self.chat_id = '-1001086397685' #отладка '-413206784'
-        self.send_message_url = f'{self.base_url}sendMessage?chat_id={self.chat_id}&text='
-        self.get_updates_url = f'{self.base_url}getUpdates?chat_id={self.chat_id}'
+#         self.chat_id = '-1001086397685' #отладка '-413206784'
+        self.send_message_url = f'{self.base_url}sendMessage?' #'#chat_id={self.chat_id}&text='
+#         self.get_updates_url = f'{self.base_url}getUpdates?chat_id={self.chat_id}'
         
         self.eng_days : List[str] = [
             'Monday',
@@ -40,44 +40,57 @@ class Bot:
     
     def load_data(self) -> None:
         with open(self.filename, 'r', encoding = 'utf-8') as f:
-            self.data : Dict[str, Dict[str, Dict[str, str]]] = json.load(f)
+#             self.data : Dict[str, Dict[str, Dict[str, str]]] = json.load(f)
+            self.data = json.load(f)
                 
             
     def send_message_with_lessons(self) -> None:
         message : str = ''
         message += self.day.upper() + '\n'
-        for lesson in self.data[self.day]:
-            message += f'\n\t{lesson} пара: \n'
-            for key, value in self.data[self.day][lesson].items():
-                message += f'\t\t{key}: {value} \n'
-#         message += '\nАвтор бота @mad_clocks\n'
-        requests.get(self.send_message_url + message)
+        for group in self.data:
+            if self.day in self.data[group]['days']:
+                for lesson in self.data[group]['days'][self.day]:
+                    message += f'\n\t{lesson} пара: \n'
+                    for key, value in self.data[group]['days'][self.day][lesson].items():
+                        message += f'\t\t{key}: {value} \n'
+        #         message += '\nАвтор бота @mad_clocks\n'
+                for chat_id in self.data[group]['chat_id']['id']:
+                    url = self.send_message_url + f'chat_id={chat_id}&text={message}'
+                print(url)
+                requests.get(url + message)
+        
+    
+    def send(self) -> None:
+        for group in self.data:
+            if self.day in self.data[group]['days']:
+                message = f'🔴🔴🔴\n{group}\t{self.day}:\n\n'
+                for lesson in self.data[group]['days'][self.day]:
+                    for key, value in self.data[group]['days'][self.day][lesson].items():
+                        message += f'\t{key}: {value}\n'
+                    message += '\n'
+                message += '\n'
+                url = self.send_message_url + f'chat_id={self.data[group]["chat_id"]["id"]}&text='
+                requests.get(url + message)
         
     
     def read_comand(self) -> None:
         print(requests.get(self.get_updates_url).json())
 
 
-# In[8]:
+# In[153]:
 
 
 #B = Bot()
 
 
-# In[9]:
+# In[154]:
 
 
 #B.load_data()
 
 
-# In[10]:
+# In[155]:
 
 
-#B.send_message_with_lessons()
-
-
-# In[116]:
-
-
-#B.read_comand()
+#B.send()
 
